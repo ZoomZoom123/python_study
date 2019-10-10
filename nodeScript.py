75df6791355ae114e0455be34709c639936f5f9b
ZoomZoom123 <56050485+ZoomZoom123@users.noreply.github.com>
2019-10-10 10:00:07 -0700

    Update nodeScript.py

--- nodeScript.py
+++ nodeScript.py
@@ -15,3 +15,33 @@ class Node:
     
 my_node = Node(44)
 print(my_node.get_value())
+
+# Create LinkedList class:
+class LinkedList:
+  def __init__(self, value=None):
+    self.head_node = Node(value)
+  
+  def get_head_node(self):
+    return self.head_node
+  
+    def insert_beginning(self, new_value):
+    new_node = Node(new_value)
+    new_node.set_next_node(self.head_node)
+    self.head_node = new_node
+# Add insert_beginning and stringify_list methods:    
+  def stringify_list(self):
+    string_list = ""
+    current_node = self.get_head_node()
+    while current_node:
+      if current_node.get_value() != None:
+        string_list += str(current_node.get_value()) + "\n"
+      current_node = current_node.get_next_node()
+    return string_list
+  
+
+# Test your code by uncommenting the statements below - did your list print to the terminal?
+ll = LinkedList(5)
+ll.insert_beginning(70)
+ll.insert_beginning(5675)
+ll.insert_beginning(90)
+print(ll.stringify_list())
